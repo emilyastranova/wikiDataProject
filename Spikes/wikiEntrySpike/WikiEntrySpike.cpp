14e179f1095e647e0153b40bb878629d31416887
Emily Astranova <emily@astralabs.dev>
2021-04-21 01:53:54 -0500

    Initial commit

---
+++ Spikes/wikiEntrySpike/WikiEntrySpike.cpp
@@ -0,0 +1,42 @@
+#include "wikiEntry.h"
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    // In the future this will be inside a loop where inputString is just the input from .dat file
+    string inputString = "13576300:156567:Hogwarts School Of Witchcraft And Wizardry";
+
+    // Test having a map of wikiEntrys
+    // map<title, wikiEntry datatype (including namespace and ID)>
+    // Using wikiEntry function parseTitle() to avoid making another wikiEntry in memory ex. "wikiEntry(inputString).title"
+    map<string, wikiEntry> entries;
+    insertEntry(entries, inputString);
+    // Some more test strings for fun
+    insertEntry(entries, "13576300:156542:The Arrival (The Twilight Zone)");
+    insertEntry(entries, "13576300:156544:Cian Ciaran");
+    insertEntry(entries, "13576300:156545:B-sides");
+    insertEntry(entries, "13576300:156546:Stepin Fetchit");
+    insertEntry(entries, "13576300:156547:Remix");
+    insertEntry(entries, "13576300:156548:Gruff Rhys");
+    insertEntry(entries, "13576300:156549:Gruff Remix");
+
+    // Get user search query
+    string userInput;
+    cout << "Enter search query: ";
+    cin >> userInput;
+
+    // showing contents:
+    map<string, wikiEntry>::iterator it = entries.begin();
+    for (it = entries.begin(); it != entries.end(); ++it) {
+        if(it->first.find(toLower(userInput)) != string::npos)
+        {
+            cout << "\nName: " << it->first << endl;
+            cout << "NS: " << it->second.pairType.first  << endl;
+            cout << "ID: " << it->second.pairType.second << endl;
+        }
+    }
+
+}
