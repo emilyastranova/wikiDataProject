58dddadade5a909f37cbf5817bb30fc1693e293f
TrumanBrown1 <browntruman@gmail.com>
2021-04-25 13:19:15 -0500

    Just changed it for readabillity, I also helped Lars install C over the weekend.

--- Spikes/wikiEntrySpike/wikiEntry.h
+++ Spikes/wikiEntrySpike/wikiEntry.h
@@ -79,12 +79,13 @@ bool wikiEntry::operator==(const wikiEntry &other) {
     }
 }
 
-bool wikiEntry::operator<(const wikiEntry &rhs) const {
-    return (
-            make_pair(stoi(this->pairType.first), stoi(this->pairType.second))
-            <
-            make_pair(stoi(rhs.pairType.first), stoi(rhs.pairType.second))
-    );
-}
+bool wikiEntry::operator<(const wikiEntry &rightHandSide) const {
+return (
+     make_pair(stoi(this->pairType.first), stoi(this->pairType.second))
+     <
+     make_pair(stoi(rightHandSide.pairType.first), stoi(rightHandSide.pairType.second))
+  );
+ }
+
 
 #endif
