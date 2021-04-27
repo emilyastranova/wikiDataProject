fc3f333e3e17003034ed965ce560ee7d6c80ebab
tyleraharrison <tyleraharrison@gmail.com>
2021-04-26 20:51:06 -0500

    Revert to old wikiEntrySpike

--- Spikes/wikiEntrySpike/WikiEntrySpike.cpp
+++ Spikes/wikiEntrySpike/WikiEntrySpike.cpp
@@ -7,9 +7,8 @@ using namespace std;
 
 int main() {
     // In the future this will be inside a loop where inputString is just the input from .dat file
-    string inputString = "13576300:156567:astarter";
+    string inputString = "13576300:156567:Hogwarts School Of Witchcraft And Wizardry";
 
-    wikiEntry Test = wikiEntry(inputString);
     // Test having a map of wikiEntrys
     // map<title, wikiEntry datatype (including namespace and ID)>
     // Using wikiEntry function parseTitle() to avoid making another wikiEntry in memory ex. "wikiEntry(inputString).title"
@@ -18,26 +17,17 @@ int main() {
     // Some more test strings for fun
     insertEntry(entries, "13576300:156542:The Arrival (The Twilight Zone)");
     insertEntry(entries, "13576300:156544:Cian Ciaran");
-    insertEntry(entries, "13576300:156547:Remix");
+    insertEntry(entries, "13576300:156545:B-sides");
     insertEntry(entries, "13576300:156546:Stepin Fetchit");
     insertEntry(entries, "13576300:156547:Remix");
-    insertEntry(entries, "13576300:156548:zender");
-
-    
+    insertEntry(entries, "13576300:156548:Gruff Rhys");
 
     // showing contents:
-    
-
     map<string, wikiEntry>::iterator it = entries.begin();
     for (it = entries.begin(); it != entries.end(); ++it) {
         cout << "\nName: " << it->first << endl;
-        if (it->second == Test) {
-            cout << "Test Found!" << endl;
-        }
-        else {
-            cout << "No Match" << endl;
-        }
         cout << "NS: " << it->second.pairType.first  << endl;
         cout << "ID: " << it->second.pairType.second << endl;
     }
+
 }
