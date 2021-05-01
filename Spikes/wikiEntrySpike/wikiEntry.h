eccba7b657baed4fb217d6b72c2c1a32bd5e851c
tyleraharrison <tyleraharrison@gmail.com>
2021-05-01 14:15:26 -0500

    Progress update on searchEngine
    Co-authored-by: TrumanBrown1 <TrumanBrown1@users.noreply.github.com>
    Co-authored-by: luanluc42 <luanluc42@users.noreply.github.com>
    Co-authored-by: luanluc42 <evanwherchek@users.noreply.github.com>

--- Spikes/wikiEntrySpike/wikiEntry.h
+++ Spikes/wikiEntrySpike/wikiEntry.h
@@ -1,10 +1,12 @@
 #ifndef H_WIKI_ENTRY_SPIKE
 #define H_WIKI_ENTRY_SPIKE
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <locale>
+#include "../../wikiData.h"
 
 using namespace std;
 class wikiEntry {
@@ -15,7 +17,9 @@ class wikiEntry {
     wikiEntry(string rawData);
     ~wikiEntry();
 
-    wikiEntry makeEntry(string ns, string id, string title);
+    void getUserInput(vector<string> &userSearchTerms);
+    void readInData(map<string, wikiEntry> &entries);
+    void printEntryVector(vector<wikiEntry> vResult);
     void search(map<string, wikiEntry> entries, string userInput, vector<wikiEntry> resultVector);
     void printEntry();
     string parseTitle(string rawData);  // Used for returning the title from a raw .dat entry string
@@ -26,8 +30,50 @@ class wikiEntry {
     pair<string, string> pairType;
     bool operator== (const wikiEntry &other);
     inline bool operator < (const wikiEntry& rhs) const;
+    istream& operator >> (wikiData& data);
 };
 
+istream& operator>>(istream& str, wikiData& data) {
+    data.readNextRow(str);
+    return str;
+}
+
+void getUserInput(vector<string> &userSearchTerms) {
+    // Credit: paddy
+    // https://stackoverflow.com/questions/14347033/reading-a-sequence-of-words-to-add-them-in-a-vector
+    //-------------------------------------------------------
+    // Get all words on one line                            |
+    cout << "Enter search term: " << flush; //              |
+    string allwords; //                                     |
+    getline(cin, allwords); //                              |
+    //                                                      |
+    // Parse words into a vector                            |
+    string word; //                                         |
+    istringstream iss(allwords); //                         |
+    while( iss >> word ) userSearchTerms.push_back(word);// |
+    //-------------------------------------------------------
+}
+
+void printEntryVector(vector<wikiEntry> vResult) {
+    // Find longest result
+    vector<wikiEntry>::iterator resultIt;
+    int longest = 0;
+    for(resultIt = vResult.begin(); resultIt != vResult.end(); ++resultIt){
+        if((signed)(resultIt->title.length()) > longest)
+            longest = resultIt->title.length();
+    }
+
+    if (vResult.empty())
+        cout << "\nNo results found" << endl;
+    else
+    {
+        cout << "\nSearch Results:\n------------------\n";
+        for(resultIt = vResult.begin(); resultIt != vResult.end(); ++resultIt){
+            cout << resultIt->title << setw(8+longest-resultIt->title.length()) << "[NS] " << resultIt->pairType.first << setw(18-resultIt->pairType.first.length()) << "[ID] " << resultIt->pairType.second << endl;
+        }
+    }
+    cout << endl;
+}
 
 string parseTitle(string rawData) {
     stringstream data(rawData);
@@ -53,7 +99,16 @@ void search(map<string, wikiEntry> &entries, string userInput, vector<wikiEntry>
     map<string, wikiEntry>::iterator it;
     for (it = entries.begin(); it != entries.end(); ++it) {
         if (it->first.find(toLower(userInput)) != string::npos) {
-            resultVector.insert(resultVector.begin(), wikiEntry(it->first, it->second.pairType.first, it->second.pairType.second));
+            resultVector.insert(resultVector.end(), wikiEntry(it->first, it->second.pairType.first, it->second.pairType.second));
+        }
+    }
+}
+
+void searchString(vector<wikiEntry> &entries, string userInput, vector<wikiEntry> &resultVector){
+
+    for (auto it : entries) {
+        if (it.title.find(toLower(userInput)) != string::npos) {
+            resultVector.insert(resultVector.end(), wikiEntry(it.title, it.pairType.first, it.pairType.second));
         }
     }
 }
@@ -66,13 +121,44 @@ void insertEntry(map<string, wikiEntry> &entries, string rawData) {
     entries.insert(pair<string, wikiEntry>(toLower(parseTitle(rawData)), wikiEntry(rawData)));
 }
 
+void readInData(map<string, wikiEntry> &entries) {
+    ifstream file("../../wikiData.dat");
+    if(file.fail()) {
+        cout << "File failed to load" << endl;
+        exit(1);
+    }
+    else
+        cout << "wikiData loaded successfully!" << endl << endl;
+
+    
+    // Get input from the file
+    wikiData row;
+    while (file >> row) {
+        insertEntry(entries, row[2], row[0], row[1]);
+    }
+}
+
+void readInData(vector<wikiEntry> &entries) {
+    ifstream file("../../wikiData.dat");
+    if(file.fail()) {
+        cout << "File failed to load" << endl;
+        exit(1);
+    }
+    else
+        cout << "wikiData loaded successfully!" << endl << endl;
+
+    
+    // Get input from the file
+    wikiData row;
+    while (file >> row) {
+        // insertEntry(entries, row[2], row[0], row[1]);
+        entries.push_back(wikiEntry(toLower(row[2]), row[0], row[1]));
+    }
+}
+
 void wikiEntry::printEntry()
 {
     cout << "Title: " << title << "\nNamespace: " << pairType.first << "\nID: " << pairType.second << endl;
-}
-
-wikiEntry makeEntry(string ns, string id, string title) {
-    return wikiEntry(title, ns, id);
 }
 
 wikiEntry::wikiEntry(string rawData) {
@@ -106,19 +192,19 @@ bool wikiEntry::operator==(const wikiEntry &other) {
 }
 
 bool wikiEntry::operator<(const wikiEntry &other) const {
-    // Lars Implementation Below:
-    if (stoi(this->pairType.first) < stoi(other.pairType.first) || stoi(this->pairType.second) < stoi(other.pairType.second)) {
-        return true;
-    } else {
-        return false;
-    }
-    
     // Truman's Implementation Below:
-    // return (
-    //   make_pair(stoi(this->pairType.first), stoi(this->pairType.second))
-    //   <
-    //     make_pair(stoi(other.pairType.first), stoi(other.pairType.second))
-    //  );
+    // if (stoi(this->pairType.first) < stoi(other.pairType.first) || stoi(this->pairType.second) < stoi(other.pairType.second)) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    
+    // Lars Implementation Below:
+    return (
+      make_pair(stoi(this->pairType.first), stoi(this->pairType.second))
+      <
+        make_pair(stoi(other.pairType.first), stoi(other.pairType.second))
+     );
 }
 
 #endif
